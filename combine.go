cc0d1e328d3b9f4326d8db08b2646cdaf3e83058
Watson-Sei <seinabehack@gmail.com>
2021-03-19 21:20:41 +0900

    combine mp4 support

--- combine.go
+++ combine.go
@@ -1,12 +1,54 @@
 package gopeg_mv4
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 	"path/filepath"
 )
 
 // 結合処理(結合ディレクトリパス, 結果保存ディレクトリパス)
+func CombineMp4(fileName, inputPath, outputPath string) error {
+	if err := WriteTxt(dirwalk(inputPath)); err != nil {
+		panic(err)
+	}
+	cmd, _ := exec.Command("ffmpeg", "-f", "concat", "-i", "/tmp/vr_memo.txt", outputPath + "/" + fileName).Output()
+	log.Println(cmd)
+	if err := RemoveTxt(); err != nil {
+		panic(err)
+	}
+	return nil
+}
 
+func WriteTxt(paths []string) error {
+	file, err := os.Create("/tmp/vr_memo.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var content string
+	for i := range paths {
+		content += fmt.Sprintf("file '%s'\n", paths[i])
+	}
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		panic(err)
+	}
+
+	return err
+}
+
+func RemoveTxt() error {
+	err := os.Remove("/tmp/vr_memo.txt")
+	if err != nil {
+		panic(err)
+	}
+	return err
+}
 
 // 結合ディレクトリパスから結合ファイルのパスを一覧取得
 // reference: https://qiita.com/tanksuzuki/items/7866768c36e13f09eedb
--- combine_test.go
+++ combine_test.go
@@ -5,6 +5,27 @@ import (
 	"testing"
 )
 
+func TestCombineMp4(t *testing.T) {
+	err := CombineMp4("main.mp4", "./test_input", "./test_output")
+	if err != nil {
+		t.Errorf("An error occurred when joining Mp4.")
+	}
+}
+
+func TestWriteTxt(t *testing.T) {
+	err := WriteTxt([]string{"test.txt","test1.txt","test2.txt"})
+	if err != nil {
+		t.Errorf("Text file creation error")
+	}
+}
+
+func TestRemoveTxt(t *testing.T) {
+	err := RemoveTxt()
+	if err != nil {
+		t.Errorf("The file was not found or there was an error deleting it.")
+	}
+}
+
 func TestWalkDir(t *testing.T) {
 	got := dirwalk("./test_input")
 	want := []string{"test_input/test.mp4", "test_input/test1.mp4", "test_input/test2.mp4"}
